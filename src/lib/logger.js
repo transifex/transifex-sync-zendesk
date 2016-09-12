b69e8c524c5a03847455ab09bf59d40ae9e0e7a8
Nikos Vasileiou <nikosv@transifex.com>
2016-09-12 15:17:57 +0300

    Add logger module

---
+++ src/lib/logger.js
@@ -0,0 +1,22 @@
+function message() {
+  return Array.prototype.join.call(arguments, ' ');
+}
+
+module.exports = {
+  info: function() {
+    if (typeof console !== 'undefined')
+      console.log('[INFO] ' + message.apply(this, arguments));
+  },
+  error: function() {
+    if (typeof console !== 'undefined')
+      console.log('[ERROR] ' + message.apply(this, arguments));
+  },
+  warn: function() {
+    if (typeof console !== 'undefined')
+      console.log('[WARN] ' + message.apply(this, arguments));
+  },
+  debug: function() {
+    if (typeof console !== 'undefined')
+      console.log('[DEBUG] ' + message.apply(this, arguments));
+  }
+};
