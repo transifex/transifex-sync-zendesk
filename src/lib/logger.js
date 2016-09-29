59ed73bc99646c2502962c854d1a151443a75144
Sofia Margariti <sofia@transifex.com>
2016-09-29 13:03:46 +0300

    Fix logger

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -11,19 +11,19 @@ function message() {
 
 module.exports = {
   info: function() {
-    if (enabled && typeof console !== 'undefined')
-      // console log '[INFO] ' + message.apply(this, arguments);
+    // if (enabled && typeof console !== 'undefined')
+    //   console log '[INFO] ' + message.apply(this, arguments);
   },
   error: function() {
-    if (typeof console !== 'undefined')
-      // console error '[ERROR] ' + message.apply(this, arguments);
+    // if (typeof console !== 'undefined')
+    //   console error '[ERROR] ' + message.apply(this, arguments);
   },
   warn: function() {
-    if (enabled && typeof console !== 'undefined')
-      // console warn '[WARN] ' + message.apply(this, arguments);
+    // if (enabled && typeof console !== 'undefined')
+    //   console warn '[WARN] ' + message.apply(this, arguments);
   },
   debug: function() {
-    if (enabled && typeof console !== 'undefined')
-      // console debug '[DEBUG] ' + message.apply(this, arguments);
+    // if (enabled && typeof console !== 'undefined')
+    //   console debug '[DEBUG] ' + message.apply(this, arguments);
   }
 };
