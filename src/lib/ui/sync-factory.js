9c65ec9306ad5e3d23b0d0fa6e2668bf58c81fc7
Sofia Margariti <sofia@transifex.com>
2016-10-12 11:25:45 +0300

    Correct subdomain

--- src/lib/ui/sync-factory.js
+++ src/lib/ui/sync-factory.js
@@ -623,12 +623,13 @@ module.exports = function(T, t, api) {
             limit = entries[t].length,
             ret = [],
             d, e, s,
+            subdomain = this.currentAccount().subdomain(),
             tx_completed, zd_object_url, tx_resource_url, zd_object_updated;
         for (var i = 0; i < limit; i++) {
           e = entries[t][i];
           s = this.store(txResource.key + e.resource_name);
           tx_completed = this.completedLanguages(s);
-          zd_object_url = "https://txtest.zendesk.com/hc/" + e.source_locale +
+          zd_object_url = "https://" + subdomain + ".zendesk.com/hc/" + e.source_locale +
             "/" + type + "/" + e.id;
           tx_resource_url = txProject.dashboard_url.replace(/\/$/, '') + '/' + e.resource_name;
           zd_object_updated = moment(e.updated_at).format(
