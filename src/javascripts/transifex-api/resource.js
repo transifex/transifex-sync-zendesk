141e4318d0b47b5eb224d2f9d7bcd25a0c2cd67f
Rigas Papathanasopoulos <rigas@transifex.com>
2019-05-07 12:37:41 +0300

    Bump version to 3.0.4

--- src/javascripts/transifex-api/resource.js
+++ src/javascripts/transifex-api/resource.js
@@ -19,7 +19,7 @@ var resource = module.exports = {
   url: '',
   inserturl: '',
   headers: {
-    'X-Source-Zendesk': 'ZendeskApp/3.0.3'
+    'X-Source-Zendesk': 'ZendeskApp/3.0.4'
   },
   username: '',
   password: '',
